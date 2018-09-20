3b1bea68dc6348e3370f00f10600666f1575967d
Parth Nagori <nagori.parth@gmail.com>
2018-09-20 15:25:44 -0400

    Player and Watcher added

---
+++ player.py
@@ -0,0 +1,86 @@
+import os
+import sys
+from kazoo.client import KazooClient
+import random
+import pickle
+import time
+
+def establish_connection(ip_port):
+  zk = KazooClient()
+  zk.start()
+  return zk
+
+def establish_directories(zk):
+  children = zk.get_children('/')
+  if 'DIC' not in children:
+    zk.create('/DIC', ephemeral=False)
+    zk.create('/DIC/online_players', ephemeral=False)
+    zk.create('/DIC/score_db', ephemeral=False)
+
+def get_online_players(zk):
+  return zk.get_children('/DIC/online_players')
+
+def post_score(zk, name, curr_score):
+  data, stat = zk.get('DIC/score_db')
+  scores = []
+  if data:
+    scores = pickle.loads(data)
+  scores.insert(0, {'name': name, 'score': curr_score})
+  zk.set('DIC/score_db', pickle.dumps(scores))
+
+def go_online(zk, name):
+  zk.create('DIC/online_players/'+name, ephemeral=True)
+
+def join(zk,name,count=None,delay=None,score=None):
+  if name in get_online_players(zk):
+    print("Player already online")
+    exit(0)
+  else:
+    go_online(zk, name)
+  if count:
+    delays = [int(abs(random.normalvariate(delay,2))) for i in range(count)]
+    scores = [int(abs(random.normalvariate(score,5))) for i in range(count)]
+    i=0
+    while (i < count):
+      post_score(zk, name, scores[i])
+      time.sleep(delays[i])
+      i+=1
+  else:
+    while(True):
+      print("Please enter a score for {}".format(name))
+      try:
+        curr_score = int(input())
+        post_score(zk, name, curr_score)
+      except ValueError:
+        print("Score is not a valid integer.")
+      
+if __name__ == '__main__':
+  count=None
+  delay=None
+  score=None
+  default_port = "2181"
+
+  args = sys.argv
+  print(args)
+  if len(args) < 3:
+    print("Please provide atleast the IP:Port and Player Name")
+    exit(0)
+  elif len(args) >= 3:
+    ip_port = args[1]
+    if ":" not in ip_port:
+      ip_port = ip_port+":"+default_port
+    name = args[2].lower()
+    if len(args) == 6:
+      count = int(args[3])
+      delay = int(args[4])
+      score = int(args[5])
+    zk = establish_connection(ip_port)
+    establish_directories(zk)
+    join(zk,name,count,delay,score)
+  else:
+    print("Invalid input format.")
+    exit(0)     
+ 
+  
+
+
---
+++ watcher.py
@@ -0,0 +1,81 @@
+import os
+import sys
+from kazoo.client import KazooClient
+from kazoo.recipe.watchers import ChildrenWatch
+from kazoo.recipe.watchers import DataWatch
+import pickle
+
+def establish_connection(ip_port):
+  zk = KazooClient()
+  zk.start()
+  return zk
+
+def establish_directories(zk):
+  children = zk.get_children('/')
+  if 'DIC' not in children:
+    zk.create('/DIC', ephemeral=False)
+    zk.create('/DIC/online_players', ephemeral=False)
+    zk.create('/DIC/score_db', ephemeral=False)
+
+def display_score(score):
+  global online_players
+  if score['name'] in online_players:
+    print("{}     {} **".format(score['name'].title(), score['score']))
+  else:
+    print("{}     {}".format(score['name'].title(), score['score']))
+
+
+def update_scoreboard():
+  global recent_scores
+  global high_scores
+  print("Most Recent Scores")
+  print("-------------------")
+  for score in recent_scores:
+    display_score(score)
+
+  print("\nHighest Scores")
+  print("-------------------")
+  for score in high_scores:
+    display_score(score)
+
+
+def scores_updated(data,stat,event=None):
+  global recent_scores
+  global high_scores
+  if data:
+    scores = pickle.loads(data)
+    recent_scores = scores[:n]
+    high_scores = sorted(scores, key=lambda k: k['score'], reverse=True)[:n]
+    update_scoreboard()
+
+def update_online_status(children):
+  global online_players
+  if set(children) != set(online_players):
+    online_players = children
+    update_scoreboard()
+    
+if __name__ == '__main__':
+  online_players = []
+  recent_scores = []
+  high_scores = []
+  n = 25
+  default_port = "2181"
+  args = sys.argv
+  if len(args) == 3:
+    ip_port = args[1]
+    if ":" not in ip_port:
+      ip_port = ip_port+":"+default_port
+    if int(args[2]) < n:
+      n = int(args[2])
+    zk = establish_connection(ip_port)
+    establish_directories(zk)
+    ChildrenWatch(zk, '/DIC/online_players', func=update_online_status)
+    DataWatch(zk, '/DIC/score_db', func=scores_updated)
+    while(True):
+      pass
+  else:
+    print("Please provide the correct arguments in the form: $python3 watcher.py <IP:PORT> <Number of records>")
+
+
+
+  
